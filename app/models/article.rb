1751f7ce49ea98750a11ec72a7ef66e8461646ee
Jacob Buckheit <jbman223@gmail.com>
2019-02-22 16:13:11 -0500

    add articles

---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+class Article < ApplicationRecord
+    belongs_to :category
+
+    validates :title, presence: true
+    validates :content, presence: true
+
+    scope :alphabetical, -> { order('title') }
+    scope :active, -> { where('active = true') }
+end
